bf42bed3d58918ff590653d2401900ab071f2b22
ielole <ieloletin@gmail.com>
2016-12-14 16:16:03 -0500

    Implement ability to upload a picture.
    - Create route to handle POST upload.
    - Alter before method to use multer for POST upload.
    - Alter create method to see what data is returned from post request
    by console logging req.body and req.file.

---
+++ app/models/upload.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const uploadSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  // _owner: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   required: true,
+  // },
+}, {
+  timestamps: true,
+});
+
+const Upload = mongoose.model('Upload', uploadSchema);
+
+module.exports = Upload;
--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -4,16 +4,33 @@
 require('dotenv').load();
 
 const s3Upload = require('../lib/aws-s3-upload');
-
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
+
+
 // function defined in lib/aws-s3-upload.js
 s3Upload(file)
-  .then(function(data) {
+  // response from resolve(data) in s3.Upload in aws-s3-upload.js
+  .then(function(response) {
     console.log("inside then block");
-    console.log("data is", data);
+    console.log("data is", response);
+    console.log("url is", response.Location);
+    return Upload.create({
+      url: response.Location,
+      title: file.title
+    });
   })
-  .catch(console.log);
+  // if above return is missing, below upload will return undefined
+  .then(function(upload){
+    console.log("mongo create was successful");
+    console.log("upload is ", upload);
+  })
+  .catch(console.error)
+  .then(function(){
+    mongoose.connection.close();
+  });
