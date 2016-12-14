d6058171d9e20f72dd89c8c83f66f06e422bde4e
ielole <ieloletin@gmail.com>
2016-12-14 10:48:06 -0500

    Refactor code for s3Upload
    Refactor code:
      - Moved AWS s3.upload function to lib/.
        - future use in API
      - Required lib/file from our command line script to ensure
        functionality wasn't affected.
      - Enables s3 upload mechanics file to be required from anywhere for
        use in API.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -1,46 +1,14 @@
 'use strict';
+// script part
 // dotenv is a module that access .env
 require('dotenv').load();
 
-const fs = require('fs');
-
-const AWS = require('aws-sdk');
-
-// invokes constructor fxn(S3) and gives us access to library
-// console.log('s3 is', s3); on the prototype
-const s3 = new AWS.S3();
-
-const mime = require('mime');
-const path = require('path');
+const s3Upload = require('../lib/aws-s3-upload');
 
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
-let contentType = mime.lookup(file.path);
-let ext = path.extname(file.path);
-// make a folder
-let folder = new Date().toISOString().split('T')[0];
-
-console.log("folder is", folder);
-console.log("contentType is", contentType);
-
-let stream = fs.createReadStream(file.path);
-let bucket = process.env.AWS_S3_BUCKET_NAME;
-
-const params = {
-  ACL: 'public-read',
-  Bucket: bucket,
-  Key: folder + '/' + file.title + ext,
-  // could also do `${folder}/${file.title}${ext}` = string interpolation
-  Body: stream,
-  ContentType: contentType,
-};
-
-s3.upload(params, function(error, data){
-  if(error){
-    console.log(error);
-  }
-  console.log(data);
-});
+// function defined in lib/aws-s3-upload.js
+s3Upload(file);
